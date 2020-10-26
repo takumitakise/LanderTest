a593afdb1e04f5cd0b3363cd64f0a4a6e830db7b
takumitakise <72957519+takumitakise@users.noreply.github.com>
2020-10-26 12:09:56 +0900

    Add files via upload

--- lander_test/scripts/lander_scan.py
+++ lander_test/scripts/lander_scan.py
@@ -2,5 +2,13 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 
-def callback_scan(msg)
+def callback_scan(msg):
+	print(msg.ranges[0])
 
+if __name__ == '__main__':
+    rospy.init_node('scan_values')
+    rate = rospy.Rate(1/1000) 
+    
+    while not rospy.is_shutdown():     
+        rospy.Subscriber('/scan', LaserScan, callback_scan)
+        rate.sleep()
